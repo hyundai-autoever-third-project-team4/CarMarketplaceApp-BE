5bf99f8f711641d6a2b206645287e31e8b54e5ff
LYJ22 <pippi478@gmail.com>
2025-01-10 10:18:28 +0900

    [feat] new car 엔티티 추가

---
+++ marketplace/src/main/java/store/carjava/marketplace/app/new_car/entity/NewCar.java
@@ -0,0 +1,25 @@
+package store.carjava.marketplace.app.new_car.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "new_car")
+public class NewCar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "average_price")
+    private Long averagePrice;
+}
