32a3d84a21f7b972b0ecb6abb620534488aa4fe9
zoonmy <pjm2571@naver.com>
2025-01-10 11:06:48 +0900

    feat: 관리자 페이지 작성 및 user 조회 서비스 구현

--- marketplace/src/main/java/store/carjava/marketplace/web/admin/controller/AdminController.java
+++ marketplace/src/main/java/store/carjava/marketplace/web/admin/controller/AdminController.java
@@ -1,14 +1,22 @@
-package store.carjava.marketplace.web.admin;
+package store.carjava.marketplace.web.admin.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import store.carjava.marketplace.app.user.entity.User;
+import store.carjava.marketplace.web.admin.service.AdminService;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
 
     @GetMapping("/admin")
     public String adminPage(Model model) {
@@ -21,9 +29,15 @@ public class AdminController {
         // 이메일 및 권한 정보 가져오기
         String email = oidcUser.getAttribute("email"); // 이메일 가져오기
 
+        // 전체 사용자 정보 조회
+        List<User> users = adminService.getAllUsers();
+
         // 모델에 사용자 정보 추가
         model.addAttribute("email", email);
         model.addAttribute("roles", authentication.getAuthorities());
+
+        // 모델에 가입한 유저 정보 추가
+        model.addAttribute("users", users);
 
         return "admin/index";
     }
