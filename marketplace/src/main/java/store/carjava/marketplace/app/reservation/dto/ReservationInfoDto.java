537814bef549648f92e53856b3045b0c48527f80
minwook <minukhan>
2025-01-13 10:32:17 +0900

    feat: Reservation Dto 구현

---
+++ marketplace/src/main/java/store/carjava/marketplace/app/reservation/dto/ReservationInfoDto.java
@@ -0,0 +1,30 @@
+package store.carjava.marketplace.app.reservation.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Builder
+public record ReservationInfoDto(
+
+        @Schema(description = "예약 ID", example = "1001")
+        Long id,
+
+        @Schema(description = "마켓플레이스 차량 ID", example = "GGI241223011164")
+        String marketplaceCarId,
+
+        @Schema(description = "예약자 유저 ID", example = "67890")
+        Long userId,
+
+        @Schema(description = "예약 날짜", example = "2023-12-01")
+        LocalDate reservationDate,
+
+        @Schema(description = "예약 시간", example = "2023-12-01T14:00:00")
+        LocalDateTime reservationTime,
+
+        @Schema(description = "예약 생성 날짜 및 시간", example = "2023-11-20T10:30:00")
+        LocalDateTime createdAt
+) {
+}
