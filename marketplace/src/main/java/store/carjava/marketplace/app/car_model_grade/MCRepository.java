c9f4ef21ea36fe1cf44400dfdc25a05311b0e70c
LYJ22 <pippi478@gmail.com>
2025-01-13 16:58:27 +0900

    [feat] 적정가격, 저렴가격 차 추천2

--- marketplace/src/main/java/store/carjava/marketplace/app/car_model_grade/MCRepository.java
+++ marketplace/src/main/java/store/carjava/marketplace/app/car_model_grade/MCRepository.java
@@ -3,10 +3,13 @@ package store.carjava.marketplace.app.car_model_grade;
 import org.springframework.data.jpa.repository.JpaRepository;
 import store.carjava.marketplace.app.marketplace_car.entity.MarketplaceCar;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MCRepository extends JpaRepository<MarketplaceCar, String>, MCRepositoryCustom {
-    Optional<MarketplaceCar> findTopByPriceLessThanEqualOrderByPriceDesc(Long price);
+    //Optional<MarketplaceCar> findTopByPriceLessThanEqualOrderByPriceDesc(Long price);
+    Optional<MarketplaceCar> findTopByCarDetails_VehicleTypeAndPriceLessThanEqualOrderByPriceDesc(String vehicle, Long price);
+    List<MarketplaceCar> findTop2ByCarDetails_VehicleTypeAndPriceLessThanEqualOrderByPriceDesc(String vehicle, Long price);
 
     boolean existsByPriceGreaterThan(Long price);
 }
--- marketplace/src/main/java/store/carjava/marketplace/app/car_model_grade/MCService.java
+++ marketplace/src/main/java/store/carjava/marketplace/app/car_model_grade/MCService.java
@@ -16,35 +16,39 @@ public class MCService {
 
     public void getRecommand(MarketplaceCarRecommandRequest request) {
         long budget = request.budget() * 10000;
+        String vehicle = request.vehicleType();
         MarketplaceCarRecommandInfoDto normal, less, over;
 
-
-        Optional<MarketplaceCar> carExist = marketplaceCarRepository.findTopByPriceLessThanEqualOrderByPriceDesc(budget);
+        // 적정, 저렴 추천
+        List<MarketplaceCar> carExist = marketplaceCarRepository.findTop2ByCarDetails_VehicleTypeAndPriceLessThanEqualOrderByPriceDesc(vehicle, budget);
         if (carExist.isEmpty()) {
-            // 예산이 너무 적은 경우 - 구매할 수 있는 차량이 없는 경우
+            // 예산 내 차가 0대인 경우 - 너무 적은 예산
             normal = MarketplaceCarRecommandInfoDto.of("해당하는 차량이 없습니다.");
             less = MarketplaceCarRecommandInfoDto.of("더 저렴한 차량이 없습니다.");
-        }
-        else{
-            // 적정 가격 추천
+        } else if (carExist.size() == 1) {
+            // 예산 내 차가 1대인 경우
+            normal = MarketplaceCarRecommandInfoDto.of(carExist.get(0));
+            less = MarketplaceCarRecommandInfoDto.of("더 저렴한 차량이 없습니다.");
+        } else{
+            // 예산 내 차가 여러 대인 경우
             long budgetLow = (long) (budget * 0.9);
             long budgetHigh = (long) (budget * 0.95);
 
-            List<MarketplaceCar> carList = marketplaceCarRepository.findMarketplaceCarList(budgetLow, budgetHigh, request.vehicleType());
+            List<MarketplaceCar> carList = marketplaceCarRepository.findMarketplaceCarList(budgetLow, budgetHigh, vehicle);
 
             // 적정 가격 범위 내에 차가 없거나 1개만 존재하는 경우
             if (carList.size() < 2) {
                 long normalPrice;
 
+                // 범위 내 차가 없음
                 if (carList.isEmpty()) {
-                    normal = MarketplaceCarRecommandInfoDto.of(carExist.get());
-                    normalPrice = carExist.get().getPrice();
-                } else {
+                    normal = MarketplaceCarRecommandInfoDto.of(carExist.get(0));
+                    normalPrice = carExist.get(0).getPrice();
+                } else {    // 범위 내 차가 1대
                     normal = MarketplaceCarRecommandInfoDto.of(carList.get(0));
                     normalPrice = carList.get(0).getPrice();
                 }
-
-                Optional<MarketplaceCar> lessCarExist = marketplaceCarRepository.findTopByPriceLessThanEqualOrderByPriceDesc(normalPrice - 1);
+                Optional<MarketplaceCar> lessCarExist = marketplaceCarRepository.findTopByCarDetails_VehicleTypeAndPriceLessThanEqualOrderByPriceDesc(vehicle, normalPrice - 1);
                 less = lessCarExist.map(MarketplaceCarRecommandInfoDto::of).orElseGet(() -> MarketplaceCarRecommandInfoDto.of("더 저렴한 차량이 없습니다."));
             }
             else{
@@ -53,17 +57,11 @@ public class MCService {
             }
         }
 
-        log.info("선택한 적정 차 이름 {}", normal.name());
-        log.info("선택한 저렴 차 이름 {}", less.name());
+        log.info("선택한 적정 차 이름 {}, 가격 {}", normal.name(), normal.price());
+        log.info("선택한 저렴 차 이름 {}, 가격 {}", less.name(), less.price());
 
-        // 예산이 너무 큰 경우 - 비싼 차량 추천 불가
-        /*boolean overCarExist = marketplaceCarRepository.existsByPriceGreaterThan(budget);
-        if(!overCarExist){
-            over = MarketplaceCarRecommandInfoDto.of("더 비싼 차량이 없습니다.");
-        }
-        else{
+        // 초과 추천
 
-        }*/
 
         return;
     }
