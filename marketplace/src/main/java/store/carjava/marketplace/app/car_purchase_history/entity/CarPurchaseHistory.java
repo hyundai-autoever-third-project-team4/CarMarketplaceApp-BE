2585f20b3e8a5003d490923e4088ea0b2b9daec6
zoonmy <pjm2571@naver.com>
2025-01-15 14:15:04 +0900

    refactor: CarPurchaseHistory 엔티티 필드 수정

--- marketplace/src/main/java/store/carjava/marketplace/app/car_purchase_history/entity/CarPurchaseHistory.java
+++ marketplace/src/main/java/store/carjava/marketplace/app/car_purchase_history/entity/CarPurchaseHistory.java
@@ -1,6 +1,5 @@
 package store.carjava.marketplace.app.car_purchase_history.entity;
 
-
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -8,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import store.carjava.marketplace.app.marketplace_car.entity.MarketplaceCar;
 import store.carjava.marketplace.app.user.entity.User;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Builder
@@ -22,13 +23,38 @@ public class CarPurchaseHistory {
     @Column(name = "id")
     private Long id;
 
+    @Column(name = "order_id", nullable = false, unique = true)
+    private String orderId;
+
+    @Column(name = "order_name", nullable = false)
+    private String orderName;
+
+    @Column(name = "approved_at", nullable = false)
+    private LocalDateTime approvedAt; // 결제일
+
+    @Column(name = "confirmed_at")
+    private LocalDateTime confirmedAt; // 확정 날짜
+
+    @Column(name = "currency", nullable = false)
+    private String currency;
+
+    @Column(name = "total_amount", nullable = false)
+    private Long totalAmount;
+
+    @Column(name = "supplied_amount", nullable = false)
+    private Long suppliedAmount;
+
+    @Column(name = "vat", nullable = false)
+    private Long vat;
+
+    @Column(name = "payment_method", nullable = false)
+    private String paymentMethod;
+
     @ManyToOne
-    @JoinColumn(name = "marketplace_car_id",nullable = false)
-    MarketplaceCar marketplaceCar;
-
+    @JoinColumn(name = "marketplace_car_id", nullable = false)
+    private MarketplaceCar marketplaceCar;
 
     @ManyToOne
-    @JoinColumn(name = "user_id",nullable = false)
-    User user;
-
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
