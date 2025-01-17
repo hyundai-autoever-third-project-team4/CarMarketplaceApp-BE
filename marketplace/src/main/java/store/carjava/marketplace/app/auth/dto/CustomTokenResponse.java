be01418dbd988d6d535766bd574f2449e97f22fd
zoonmy <pjm2571@naver.com>
2025-01-17 09:21:07 +0900

    refactor: login 응답 수정

--- marketplace/src/main/java/store/carjava/marketplace/app/auth/dto/CustomTokenResponse.java
+++ marketplace/src/main/java/store/carjava/marketplace/app/auth/dto/CustomTokenResponse.java
@@ -1,11 +1,26 @@
 package store.carjava.marketplace.app.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import store.carjava.marketplace.app.user.entity.User;
 
 public record CustomTokenResponse(
         @JsonProperty("access_token") String accessToken,
         @JsonProperty("refresh_token") String refreshToken,
         Long userId,
-        String email
+        String email,
+        String name,
+        String phone,
+        String address
 ) {
+    public static CustomTokenResponse of(String accessToken, String refreshToken, User user) {
+        return new CustomTokenResponse(
+                accessToken,
+                refreshToken,
+                user.getId(),
+                user.getEmail(),
+                user.getName(),
+                user.getPhone(),
+                user.getAddress()
+        );
+    }
 }
