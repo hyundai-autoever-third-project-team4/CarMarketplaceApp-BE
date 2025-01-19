34cabf1d74c903230c534deddbc5557ee009a4d8
pjm2571 <pjm2571@naver.com>
2025-01-19 18:15:27 +0900

    feat: 사용자 역할별 엔드포인트 토픽 구현

--- marketplace/src/main/java/store/carjava/marketplace/socket/controller/ChatController.java
+++ marketplace/src/main/java/store/carjava/marketplace/socket/controller/ChatController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import store.carjava.marketplace.socket.dto.ChatMessage;
+import store.carjava.marketplace.app.user.entity.User;
+import store.carjava.marketplace.app.user.exception.UserIdNotFoundException;
+import store.carjava.marketplace.app.user.repository.UserRepository;
+import store.carjava.marketplace.common.util.user.UserResolver;
+import store.carjava.marketplace.socket.dto.ChatMessageRequest;
+import store.carjava.marketplace.socket.dto.ChatMessageDto;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,13 +18,27 @@ import store.carjava.marketplace.socket.dto.ChatMessage;
 public class ChatController {
 
     private final RabbitTemplate rabbitTemplate;
+    private final UserRepository userRepository;
 
     @MessageMapping("/chat.send")
-    public void sendMessage(ChatMessage message) {
+    public void sendMessage(ChatMessageRequest messageRequest) {
+        User sender = userRepository.findById(messageRequest.senderId())
+                .orElseThrow(UserIdNotFoundException::new);
+
+        boolean isAdmin = false;
+
+        if (sender.getRole().equals("ROLE_ADMIN")) {
+            isAdmin = true;
+        }
+
+        log.info("메세지 송신자의 ROLE : {}", isAdmin ? "ADMIN" : "USER");
+
+        ChatMessageDto chatMessageDto = ChatMessageDto.of(messageRequest, isAdmin);
+
         // 로그로 메시지 내용 출력
-        log.info("Received message: {}", message);
+        log.info("서버가 받은 메세지 : {}", chatMessageDto);
 
         // Rabbit MQ에 메세지 전송
-        rabbitTemplate.convertAndSend("chat.queue", message);
+        rabbitTemplate.convertAndSend("chat.queue", chatMessageDto);
     }
 }
--- marketplace/src/main/java/store/carjava/marketplace/socket/service/ChatMessageListener.java
+++ marketplace/src/main/java/store/carjava/marketplace/socket/service/ChatMessageListener.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import store.carjava.marketplace.socket.dto.ChatMessage;
+import store.carjava.marketplace.socket.dto.ChatMessageDto;
 
 @Component
 @RequiredArgsConstructor
@@ -14,10 +14,15 @@ public class ChatMessageListener {
     private final SimpMessagingTemplate messagingTemplate;
 
     @RabbitListener(queues = "chat.queue")
-    public void handleMessage(ChatMessage message) {
-        log.info("Listened : {}", message);
+    public void handleMessage(ChatMessageDto message) {
+        log.info("Rabbit MQ 메세징 큐에서 받은 메세지 : {}", message);
 
-        // WebSocket을 통해 메시지를 전송
-        messagingTemplate.convertAndSend("/queue/" + message.senderId(), message);
+        if (message.isAdmin()) {
+            log.info("[ADMIN] ⇒ [USER # {}]", message.receiverId());
+            messagingTemplate.convertAndSend("/queue/user-" + message.senderId(), message);
+        } else {
+            log.info("[USER] ⇒ [ADMIN]");
+            messagingTemplate.convertAndSend("/topic/admin", message);
+        }
     }
 }
