d7d572e2f28f4db6c0d74a5158ca9fb95e6cb462
LYJ22 <pippi478@gmail.com>
2025-01-19 22:35:30 +0900

    [feat] 토큰 생성, 갱신, 삭제 api

--- marketplace/src/main/java/store/carjava/marketplace/firebase/repository/FCMTokenRepository.java
+++ marketplace/src/main/java/store/carjava/marketplace/firebase/repository/FCMTokenRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import store.carjava.marketplace.firebase.entity.FCMToken;
 
+import java.util.Optional;
+
 @Repository
 public interface FCMTokenRepository extends JpaRepository<FCMToken, Long> {
-
+    Optional<FCMToken> findByUserIdAndFcmToken(Long userId, String token);
+    void deleteByFcmToken(String token);
 }
