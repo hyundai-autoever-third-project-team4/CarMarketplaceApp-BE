b377dfa97b682d81b1c10399628d6fbfffb4b143
zoonmy <pjm2571@naver.com>
2025-01-20 17:22:31 +0900

    refactor: html, 수정

--- marketplace/src/main/java/store/carjava/marketplace/MarketplaceApplication.java
+++ marketplace/src/main/java/store/carjava/marketplace/MarketplaceApplication.java
@@ -1,10 +1,12 @@
 package store.carjava.marketplace;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableRabbit
 public class MarketplaceApplication {
 
     public static void main(String[] args) {
